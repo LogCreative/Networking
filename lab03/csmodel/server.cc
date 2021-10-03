049bedf797b6f7481f7345439cccc3ef44ac00df
LogCreative <lizilong123456@live.cn>
2021-10-03 10:50:42 +0800

    lab03 server proto

--- lab03/csmodel/server.cc
+++ lab03/csmodel/server.cc
@@ -1,66 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdio.h> // standard input and output library
+#include <stdlib.h> // this includes functions regarding memory allocation
+#include <string.h> // contains string functions
+#include <errno.h> //It defines macros for reporting and retrieving error conditions through error codes
+#include <time.h> //contains various functions for manipulating date and time
+#include <unistd.h> //contains various constants
+#include <sys/types.h> //contains a number of basic derived types that should be used whenever appropriate
+#include <arpa/inet.h> // defines in_addr structure
+#include <sys/socket.h> // for socket creation
+#include <netinet/in.h> //contains constants and structures needed for internet domain addresses
 #include <pthread.h>
 
-pthread_mutex_t mutex;
- 
-void *serve(void* param)
-{
-	int this_server = rand();
-
-    time_t clock;
+void* send_thread(void* conn_void_ptr){
+	int clintConnt = *((int*)conn_void_ptr);
+	pthread_detach(pthread_self());
+	free(conn_void_ptr);
+	
 	char dataSending[1025]; // Actually this is called packet in Network Communication, which contain data and send through.
-	int clintListn = 0, clintConnt = 0;
+	memset(dataSending, '0', sizeof(dataSending));
+
+	time_t clock;
+	clock = time(NULL);
+	snprintf(dataSending, sizeof(dataSending), "%.24s\r\n", ctime(&clock)); // Printing successful message
+	write(clintConnt, dataSending, strlen(dataSending));
+
+	close(clintConnt);
+}
+
+int main()
+{
+	int clintListn = 0;
 	struct sockaddr_in ipOfServer;
 	clintListn = socket(AF_INET, SOCK_STREAM, 0); // creating socket
 	memset(&ipOfServer, '0', sizeof(ipOfServer));
-	memset(dataSending, '0', sizeof(dataSending));
 	ipOfServer.sin_family = AF_INET;
 	ipOfServer.sin_addr.s_addr = htonl(INADDR_ANY);
 	ipOfServer.sin_port = htons(2680); 		// this is the port number of running server
 	bind(clintListn, (struct sockaddr*)&ipOfServer , sizeof(ipOfServer));
 	listen(clintListn , 1);
  
+	int* clintConnt;
+	pthread_t th;
+
 	while(1)
 	{
-		pthread_mutex_lock(&mutex);
-		printf("\n\nHi,I am running server.Some Client hit me\nServed by Number %d\n", this_server); // whenever a request from client came. It will be processed here.
-		clintConnt = accept(clintListn, (struct sockaddr*)NULL, NULL);
- 
-		clock = time(NULL);
-		snprintf(dataSending, sizeof(dataSending), "%.24s\r\n", ctime(&clock)); // Printing successful message
-		write(clintConnt, dataSending, strlen(dataSending));
-		// Assume that it takes 10 seconds of time.
-		// sleep(10);
+		printf("\n\nHi,I am running server.Some Client hit me\n"); // whenever a request from client came. It will be processed here.
+		
+		clintConnt = (int*)malloc(sizeof(int));
+		*clintConnt = accept(clintListn, (struct sockaddr*)NULL, NULL);
 
-        close(clintConnt);
-		pthread_mutex_unlock(&mutex);
-        sleep(1);
+		pthread_create(&th, NULL, send_thread, clintConnt);
     }
  
-    return NULL;
-}
-
-int main(){
-
-    pthread_mutex_init(&mutex,NULL);
-
-    int p_count = 5;
-
-    pthread_t* pbee = (pthread_t *) malloc(p_count*(sizeof(pthread_t)));
-    for(int i = 0; i < p_count; ++i)
-        pthread_create(&pbee[i], NULL, serve, NULL);
-
-	for(int i = 0; i < p_count; ++i)
-        pthread_join(pbee[i],NULL);
-
     return 0;
 }
